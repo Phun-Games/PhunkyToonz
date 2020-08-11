1627fccb5d6ed403325781e6b6de61a7da24d119
nydkim <nydkim@gmail.com>
2020-08-11 11:28:19 -0400

    initial webpack setup complete

---
+++ client/components/App.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+
+const App = () => {
+  return (<h1>hello</h1>)
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.jsx',
+  // entry: 'client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    // path: 'build',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './client',
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    publicPath: '/build/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+
+
+}
